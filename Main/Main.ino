debfff06a0a2ac4512c3fededf165d3c8ca06f40
florinbuda85 <florinbuda85@gmail.com>
2018-06-02 15:08:21 +0300

    Create Main program

---
+++ Main/Main.ino
@@ -0,0 +1,20 @@
+
+int motor1_forward = D0;
+int motor1_backward = D1;
+
+int motor2_left = D2;
+int motor2_right = D3;
+
+int carSpeed = 70;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(motor1_forward, OUTPUT);
+  pinMode(motor1_backward, OUTPUT);
+  pinMode(motor2_left, OUTPUT);
+  pinMode(motor2_right, OUTPUT);
+}
+
+
+void loop() {
+}
