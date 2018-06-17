ad204c1bd8a0032e0eb48e2ea9ee6fd2142f30b4
florinbuda85 <florinbuda85@gmail.com>
2018-06-17 15:35:01 +0300

    webserver on arduino

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,3 +1,23 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid     = "virus33";
+const char* password = "testtest";  
+
+WiFiClient client;
+
+ESP8266WebServer server(80);
+
+void handleRoot();              // function prototypes for HTTP handlers
+void handleForward();
+void handleStop();
+void handleAccelerate();
+void handleDecelerate();
+void handleTurnRight();
+void handleTurnLeft();
+void handleNotFound();
+
 
 int motor1_forward = D0;
 int motor1_backward = D1;
@@ -5,34 +25,99 @@ int motor1_backward = D1;
 int motor2_left = D2;
 int motor2_right = D3;
 
-int carSpeed = 70;
+int carSpeed = 250;
+int maxSpeed = 1023;
 
 void setup() {
+
+  Serial.begin(115200);
+  
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(motor1_forward, OUTPUT);
   pinMode(motor1_backward, OUTPUT);
   pinMode(motor2_left, OUTPUT);
   pinMode(motor2_right, OUTPUT);
+
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  server.on("/", HTTP_GET, handleRoot); 
+  server.on("/W", HTTP_GET, handleForward); 
+  server.on("/S", HTTP_GET, handleStop); 
+  server.on("/Q", HTTP_GET, handleAccelerate); 
+  server.on("/E", HTTP_GET, handleDecelerate); 
+  server.on("/D", HTTP_GET, handleTurnRight); 
+  server.on("/A", HTTP_GET, handleTurnLeft); 
+  server.begin();
+  
 }
 
+void handleRoot() {
+  server.send(200, "text/html", "" + String(carSpeed));
+}
+
+void handleForward() {
+  car_forward();
+  server.sendHeader("Location","/");
+  server.send(303);
+  //server.send(200, "text/html", "" + String(carSpeed));
+}
+
+void handleStop() {
+  car_stop();
+  server.sendHeader("Location","/");
+  server.send(303); 
+}
+
+void handleAccelerate() {
+  accelerate();
+  server.sendHeader("Location","/");
+  server.send(303); 
+}
+
+void handleDecelerate() {
+  decelerate();
+  server.sendHeader("Location","/");
+  server.send(303); 
+}
+
+void handleTurnRight() {
+  turn_right();
+  server.sendHeader("Location","/");
+  server.send(303); 
+}
+
+void handleTurnLeft() {
+  turn_left();
+  server.sendHeader("Location","/");
+  server.send(303); 
+}
 
 void loop() {
+  server.handleClient();
 }
 
 void turn_right() {
-	digitalWrite(motor2_right, HIGH);
-  	delay(500);
-  	digitalWrite(motor2_right, LOW);
+  digitalWrite(motor2_right, HIGH);
+  delay(700);
+  digitalWrite(motor2_right, LOW);
 }
 
 void turn_left() {
 	digitalWrite(motor2_left, HIGH);
-  	delay(500);
-  	digitalWrite(motor2_left, LOW);
+  delay(700);
+  digitalWrite(motor2_left, LOW);
 }
 
-void car_start() {
+void car_forward() {
 	analogWrite(motor1_forward, carSpeed);	
+  //digitalWrite(motor1_forward, 1);  
+  Serial.println(carSpeed);
 }
 
 void car_stop() {
@@ -41,14 +126,17 @@ void car_stop() {
 }
 
 void accelerate() {
-	if (carSpeed < 255) {
-		carSpeed += 10;
+  carSpeed += 50;
+	if (carSpeed > maxSpeed) {
+		carSpeed = maxSpeed;
 	}
+ car_forward();
 }
 
 void decelerate() {
-	carSpeed -= 10;
-	if (carSpeed < 0) {
+	carSpeed -= 5;
+	if (carSpeed < 50) {
 		carSpeed = 0;
 	}
+ car_forward();
 }
